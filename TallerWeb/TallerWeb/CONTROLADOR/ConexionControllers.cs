a18d260d15af086010918c9cef0c9539078fd58b
oscartitan <oscartitan@gmail.com>
2017-12-07 20:52:01 -0500

    Se crear el controlardor de conexion

---
+++ TallerWeb/TallerWeb/CONTROLADOR/ConexionControllers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerWeb.CONTROLADOR
+{
+    public class ConexionControllers
+    {
+    }
+}
