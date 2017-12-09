59030b024404b304a29dd26387ca6226ad065f53
oscartitan <oscartitan@gmail.com>
2017-12-09 14:44:25 -0500

    OraConsulta (select)

--- TallerPowVive/TallerPowVive/CONTROLADOR/ConexionControllers.cs
+++ TallerPowVive/TallerPowVive/CONTROLADOR/ConexionControllers.cs
@@ -16,5 +16,30 @@ namespace TallerPowVive.CONTROLADOR
         }
 
 
+        public DataSet OraConsulta(String SQL)
+        {
+            OracleConnection conexion = new OracleConnection(this.cadenaConexionOra());
+            //OracleConnection conexion = new OracleConnection("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.1.1.2)(PORT=1521))(CONNECT_DATA=(SERVER=dedicated)(SERVICE_NAME=XE)));User ID=TALLER;Password=admin; Unicode=true");
+            OracleCommand comando = new OracleCommand(SQL, conexion);
+            OracleDataAdapter datos = new OracleDataAdapter(comando);
+            DataSet tabla = new DataSet();
+            //Excepciones (bloques: try - catch - finally)
+            try // Se ejecuta hasta que termine o se genere una excepcion
+            {
+                conexion.Open();
+                datos.Fill(tabla);
+            }
+            catch (Exception) // atrapa la excepcion que hereden de System.Exception
+            {
+                return null;
+            }
+            finally  // se ejecuta si se produce o no una excepcion 
+            {
+                conexion.Close();
+                comando.Dispose();
+            }
+            return tabla;
+        }
+
     }
 }
