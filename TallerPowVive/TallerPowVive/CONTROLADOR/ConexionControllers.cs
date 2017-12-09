cf91ad35683cd4dfabc792f2c9718c76e2845075
oscartitan <oscartitan@gmail.com>
2017-12-09 11:53:56 -0500

    cadena weconfig

---
+++ TallerPowVive/TallerPowVive/CONTROLADOR/ConexionControllers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TallerPowVive.CONTROLADOR
+{
+    public class ConexionControllers
+    {
+
+        public string cadenaConexionOra()
+        {
+            return ConfigurationManager.ConnectionStrings["conexion_oracle"].ConnectionString;
+               
+        }
+
+
+    }
+}
