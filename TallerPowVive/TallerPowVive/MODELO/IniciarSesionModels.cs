4aafb2dbbbee847bc6f71513e290244c065d69e1
oscartitan <oscartitan@gmail.com>
2017-12-09 15:35:05 -0500

    validacionUsuario MODELO

---
+++ TallerPowVive/TallerPowVive/MODELO/IniciarSesionModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using TallerPowVive.CONTROLADOR;
+
+namespace TallerPowVive.MODELO
+{
+    public class IniciarSesionModels
+    {
+        ConexionControllers dataload = new ConexionControllers();
+        String Sql = String.Empty;
+
+        internal DataSet validacionUsuario(string usuario, string contrasena)
+        {            
+            Sql = "select NOMBRE_USUARIO from TUSUARIOS where USUARIO_USUARIO = '" + usuario + "' and CONTRASENA_USUARIO = '" + contrasena + "'";
+            return dataload.OraConsulta(Sql);
+        }
+    }
+}
