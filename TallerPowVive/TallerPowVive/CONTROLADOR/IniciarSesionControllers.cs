0eae4b4dc571566eb66a63c3bbea8e18288a1aeb
oscartitan <oscartitan@gmail.com>
2017-12-09 15:47:33 -0500

    IniciarSesionControllers MODELO

---
+++ TallerPowVive/TallerPowVive/CONTROLADOR/IniciarSesionControllers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using TallerPowVive.MODELO;
+
+namespace TallerPowVive.CONTROLADOR
+{
+    public class IniciarSesionControllers
+    {
+        // metodo para validar el usuario y contraseña
+        public static DataSet validacion(string usuario, string contrasena)
+        {
+            IniciarSesionModels usu = new IniciarSesionModels();
+
+            try
+            {
+                return usu.validacionUsuario(usuario, contrasena);
+            }
+            catch (Exception ex)
+            {
+                throw; // para lanzar la exception o complementar la capturada
+            }
+        }
+
+    }
+}
